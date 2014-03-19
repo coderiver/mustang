20e810fdfc3998988ac2a1979463505f0c6dd897
Denis Pismenny <denis.pismenny@gmail.com>
2014-03-19 12:12:41 +0200

    add feedback form

--- js/common.js
+++ js/common.js
@@ -1,5 +1,42 @@
 $(document).ready(function() {
 
+	//click document
+	$(document).click(function() {
+		$('.js-select').removeClass('is-open');
+	});
+
+	//select
+	function select() {
+		var el = $('.js-select'),
+				el_title = el.children("span"),
+				item = el.find('li'),
+				input = el.find(".js-select-input"),
+				el_text = el.find(".js-select-text"),
+				checkbox = el.find(".checkbox"),
+				list = el.find('.js-select-drop');
+
+		el_title.click(function(event){
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$(this).parent().addClass('is-open');
+			}
+			event.stopPropagation();
+		});
+		checkbox.click(function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			el.find('li').removeClass("is-selected");
+			$(this).addClass("is-selected");
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select").find(".js-select-text").text(text);
+			$(this).parents(".js-select").find(".js-select-input").val(id);
+		});
+	} select();
 
 	$('.js-mainslider').cycle({
 		fx: "scrollHorz",
@@ -14,34 +51,5 @@ $(document).ready(function() {
 		slides: ".main-block__item",
 		pagerTemplate: "<span></span>"
 	});
-
-	// $.fn.tabSample = function(){
-	// 	$(this).each(function(idx, el){
-	// 		var $this = $(this);
-	// 		$this.delegate('.tabs__nav li', 'click', function(event){
-	// 			var index = $this.find(".tabs__nav li").index(event.target);
-	// 			$this.find(".tabs__items div").removeClass('active');
-	// 			var item = $this.find(".tabs__items div:eq(" + index + ")").addClass('active');
-	// 			console.log(item, index);
-	// 		});
-	// 		$this.find('.tabs__items div:eq(0)').addClass('active');
-	// 	});
-	// };
-	// $(".tabs").tabSample();
-
-	// function tabs() {
-	// 	$('.tabs__nav a').on('click', function(event) {
-	// 		// alert("I'm clicked, dude!")
-	// 		if ($('.tabs__nav li').hasClass('is-active')) {
-	// 			$('.tabs__nav li').removeClass('is-active');
-	// 			$(this).parent().addClass('is-active');
-	// 		} else{
-	// 			var index = $(this).parent().index(event.target);
-	// 			console.log(index);
-	// 			$(this).parent().addClass('is-active');
-	// 		}
-	// 		return false;
-	// 	});
-	// } tabs();
 
 });
